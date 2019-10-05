336c7641934c066465294e23f15bab081e9efa5b
Vankka <20226336+Vankka@users.noreply.github.com>
2019-10-05 15:19:09 +0300

    Economy support

--- src/main/java/com/djrapitops/extension/EssentialsExtension.java
+++ src/main/java/com/djrapitops/extension/EssentialsExtension.java
@@ -25,6 +25,7 @@ package com.djrapitops.extension;
 import com.djrapitops.plan.extension.CallEvents;
 import com.djrapitops.plan.extension.DataExtension;
 import com.djrapitops.plan.extension.FormatType;
+import com.djrapitops.plan.extension.NotReadyException;
 import com.djrapitops.plan.extension.annotation.*;
 import com.djrapitops.plan.extension.icon.Color;
 import com.djrapitops.plan.extension.icon.Family;
@@ -32,12 +33,16 @@ import com.earth2me.essentials.Essentials;
 import com.earth2me.essentials.User;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.math.BigDecimal;
 import java.util.*;
 
 /**
  * DataExtension for Essentials plugin.
  *
+ * Economy Support by Vankka
+ *
  * @author Rsl1122
+ * @author Vankka
  */
 @PluginInfo(name = "Essentials", iconName = "flask", iconFamily = Family.SOLID, color = Color.DEEP_ORANGE)
 public class EssentialsExtension implements DataExtension {
@@ -55,29 +60,22 @@ public class EssentialsExtension implements DataExtension {
     @Override
     public CallEvents[] callExtensionMethodsOn() {
         return new CallEvents[]{
-                CallEvents.PLAYER_JOIN, CallEvents.PLAYER_LEAVE
+                CallEvents.PLAYER_JOIN, CallEvents.PLAYER_LEAVE,
+                CallEvents.SERVER_EXTENSION_REGISTER, CallEvents.SERVER_PERIODICAL
         };
     }
 
-    @BooleanProvider(
-            text = "",
-            conditionName = "hasUser",
-            hidden = true
-    )
-    public boolean hasUser(UUID playerUUID) {
-        return essentials.getUserMap().userExists(playerUUID);
+    private User getUser(UUID playerUUID) {
+        User user = essentials.getUser(playerUUID);
+        if (user == null) throw new NotReadyException();
+        return user;
     }
 
-    private Optional<User> getUser(UUID playerUUID) {
-        return Optional.ofNullable(essentials.getUser(playerUUID));
-    }
-
-    @Conditional("hasUser")
     @BooleanProvider(
             text = "Jailed", iconName = "ban", priority = 100, iconColor = Color.DEEP_ORANGE, conditionName = "isJailed"
     )
     public boolean isJailed(UUID playerUUID) {
-        return getUser(playerUUID).map(User::isJailed).orElse(false);
+        return getUser(playerUUID).isJailed();
     }
 
     @Conditional("isJailed")
@@ -85,15 +83,14 @@ public class EssentialsExtension implements DataExtension {
             text = "Jail Expires", iconName = "calendar-times", priority = 99, iconFamily = Family.REGULAR, iconColor = Color.DEEP_ORANGE, format = FormatType.DATE_SECOND
     )
     public long jailTimeout(UUID playerUUID) {
-        return getUser(playerUUID).map(User::getJailTimeout).orElse(-1L);
+        return getUser(playerUUID).getJailTimeout();
     }
 
-    @Conditional("hasUser")
     @BooleanProvider(
             text = "Muted", iconName = "bell-slash", priority = 80, iconColor = Color.BLUE_GREY, conditionName = "isMuted"
     )
     public boolean isMuted(UUID playerUUID) {
-        return getUser(playerUUID).map(User::isMuted).orElse(false);
+        return getUser(playerUUID).isMuted();
     }
 
     @Conditional("isMuted")
@@ -101,15 +98,14 @@ public class EssentialsExtension implements DataExtension {
             text = "Mute Expires", iconName = "calendar-times", priority = 79, iconFamily = Family.REGULAR, iconColor = Color.BLUE_GREY, format = FormatType.DATE_SECOND
     )
     public long muteTimeout(UUID playerUUID) {
-        return getUser(playerUUID).map(User::getJailTimeout).orElse(-1L);
+        return getUser(playerUUID).getJailTimeout();
     }
 
-    @Conditional("hasUser")
     @StringProvider(
             text = "homes", iconName = "home", priority = 70, iconColor = Color.GREEN
     )
     public String playerHomes(UUID playerUUID) {
-        List<String> homes = getUser(playerUUID).map(User::getHomes).orElse(new ArrayList<>());
+        List<String> homes = getUser(playerUUID).getHomes();
         if (homes.isEmpty()) {
             return "-";
         }
@@ -127,6 +123,46 @@ public class EssentialsExtension implements DataExtension {
         }
 
         return homeString.toString();
+    }
+
+    @StringProvider(
+            text = "Balance", iconName = "coins", priority = 60, iconColor = Color.GREEN
+    )
+    public String balance(UUID playerUUID) {
+        if (essentials.getSettings().isEcoDisabled()) {
+            return null;
+        }
+
+        return getUser(playerUUID).getMoney().toString();
+    }
+
+    @StringProvider(
+            text = "Total Server Balance", iconName = "coins", priority = 50, iconColor = Color.GREEN
+    )
+    public String totalServerBalance() {
+        if (essentials.getSettings().isEcoDisabled()) {
+            return null;
+        }
+
+        // Source: EssentialsX
+        // https://github.com/EssentialsX/Essentials/blob/2.x/Essentials/src/com/earth2me/essentials/commands/Commandbalancetop.java
+        BigDecimal totalMoney = BigDecimal.ZERO;
+        for (UUID uuid : essentials.getUserMap().getAllUniqueUsers()) {
+            final User user = essentials.getUserMap().getUser(uuid);
+            if (user != null) {
+                if (!essentials.getSettings().isNpcsInBalanceRanking() && user.isNPC()) {
+                    // Don't list NPCs in output
+                    continue;
+                }
+                if (!user.isAuthorized("essentials.balancetop.exclude")) {
+                    final BigDecimal userMoney = user.getMoney();
+                    user.updateMoneyCache(userMoney);
+                    totalMoney = totalMoney.add(userMoney);
+                }
+            }
+        }
+
+        return totalMoney.toString();
     }
 
 }
